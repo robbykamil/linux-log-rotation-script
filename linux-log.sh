9cff8cb452bf0928cbac62fed74e463f79117f52
robbykamil <robbykamil99@gmail.com>
2026-06-09 16:46:26 +0000

    added date on log file name and merge the output with curly braces

--- linux-log.sh
+++ linux-log.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # The log file name
-logfilename="systemavailability.log"
+logfilename="systemavailability$(date +%Y%m%d).log"
+
 # Create the log file if does not exist
-if [ ! -f "$logfilename" ]; 
+if [ ! -f "$logfilename" ];
   then touch "$logfilename"
 fi
+
+{
 # Get the data and write out to the log
-echo "==========================================" >> "$logfilename"
+echo "=========================================="
+
 # Current time (Format: YYYY-MM-DD HH:MM:SS)
-echo "Time: $(date '+%Y-%m-%d %H:%M:%S')" >> "$logfilename"
+echo "Time: $(date '+%Y-%m-%d %H:%M:%S')"
+
 # List of top 5 processes
-echo "Top 5 Proses CPU:" >> "$logfilename" 
-ps aux --sort=-%cpu | awk 'NR>1 {print $1, $3 "%", $11}' | head -n 5 >> "$logfilename"
+echo "Top 5 Proses CPU:"
+ps aux --sort=-%cpu | awk 'NR>1 {print $1, $3 "%", $11}' | head -n 5
+
 #Memory usage
-echo -n "Memory usage: " >> "$logfilename" 
-free | awk '/Mem:/ {printf "%.2f%%\n", $3/$2 * 100}' >> "$logfilename"
-echo "==========================================" >> "$logfilename"
+echo -n "Memory usage: "
+free | awk '/Mem:/ {printf "%.2f%%\n", $3/$2 * 100}'
+echo "=========================================="
+} >> "$logfilename"
