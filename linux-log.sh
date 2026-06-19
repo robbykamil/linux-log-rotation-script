f6257c7eca382740454b3c4e12156d5b4220c848
robbykamil <robbykamil99@gmail.com>
2026-06-19 15:05:53 +0000

    feat: redirect log outputs to a dedicated output folder

--- linux-log.sh
+++ linux-log.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Define the output directory
+output_dir="output_logs"
+mkdir -p "$output_dir"
+
 # The log file name
-logfilename="systemavailability$(date +%Y%m%d).log"
+logfilename="$output_dir/systemavailability$(date +%Y%m%d).log"
 
 # Create the log file if does not exist
 if [ ! -f "$logfilename" ];
@@ -31,24 +35,24 @@ size_checking=$(stat -c%s "$logfilename")
 if [ "$size_checking" -gt "$filesize" ]; then 
   countfile=1
 
-  while [ -f "systemavailability$(date +%Y%m%d)-$countfile.log" ]; do
+  while [ -f "$output_dir/systemavailability$(date +%Y%m%d)-$countfile.log" ]; do
     countfile=$((countfile + 1))
   done
 
-  mv "$logfilename" "systemavailability$(date +%Y%m%d)-$countfile.log"
+  mv "$logfilename" "$output_dir/systemavailability$(date +%Y%m%d)-$countfile.log"
   touch "$logfilename"
 fi
 
 #zip log file yesterday
 yesterday=$(date -d "yesterday" "+%Y%m%d") #date format of yesterday
 
-zipfilename="systemavailability-$yesterday.zip"
+zipfilename="$output_dir/systemavailability-$yesterday.zip"
 
-if ls systemavailability$yesterday*log >/dev/null 2>&1 && [ ! -f "$zipfilename" ]; then
-  zip -m "$zipfilename" systemavailability$yesterday*log
+if ls $output_dir/systemavailability$yesterday*log >/dev/null 2>&1 && [ ! -f "$zipfilename" ]; then
+  zip -j -m "$zipfilename" $output_dir/systemavailability$yesterday*log
 fi
 
 #set the period of archive retention (days)
 retention=30
 
-find . -maxdepth 1 -type f -name "*.zip" -mtime +"$retention" -delete
+find $output_dir -maxdepth 1 -type f -name "*.zip" -mtime +"$retention" -delete
